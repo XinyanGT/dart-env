bc789aa65bf550305020aac5c72bd3e04d72b044
xyan43 <voidpointeryan@gmail.com>
2019-10-20 20:36:10 -0400

    Disable viewer for CartPole and Reacher

--- gym/envs/dart/cart_pole.py
+++ gym/envs/dart/cart_pole.py
@@ -3,10 +3,10 @@ from gym import utils
 from gym.envs.dart import dart_env
 
 class DartCartPoleEnv(dart_env.DartEnv, utils.EzPickle):
-    def __init__(self):
+    def __init__(self, disableViewer=True):
         control_bounds = np.array([[1.0],[-1.0]])
         self.action_scale = 100
-        dart_env.DartEnv.__init__(self, 'cartpole.skel', 2, 4, control_bounds, dt=0.02, disableViewer=False)
+        dart_env.DartEnv.__init__(self, 'cartpole.skel', 2, 4, control_bounds, dt=0.02, disableViewer=disableViewer)
         utils.EzPickle.__init__(self)
 
     def step(self, a):
